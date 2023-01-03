762b0ee3313ea710a9820a87b5bd1e78ec884509
baro35 <baranselcuk35@gmail.com>
2023-01-04 02:24:09 +0300

    queries

--- src/main/java/com/example/foodcommercial/repositories/FavoriteRestaurantReposistory.java
+++ src/main/java/com/example/foodcommercial/repositories/FavoriteRestaurantReposistory.java
@@ -4,10 +4,12 @@ import com.example.foodcommercial.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.foodcommercial.entities.FavoriteRestaurants;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FavoriteRestaurantReposistory extends JpaRepository<FavoriteRestaurants, Long> {
+    @Query("SELECT f_ FROM User u_ JOIN FavoriteRestaurants f_ on u_.id=f_.user.id WHERE u_ = :user")
     List<FavoriteRestaurants> getFavoriteRestaurantsByUser(User user);
     FavoriteRestaurants getFavoriteRestaurantsById(Long id);
 
--- src/main/java/com/example/foodcommercial/repositories/OrderRepository.java
+++ src/main/java/com/example/foodcommercial/repositories/OrderRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import com.example.foodcommercial.entities.Order;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-	
+
+    @Query("SELECT o_ FROM User u_ JOIN Order o_ on u_.id=o_.user.id WHERE u_ = :user")
     List<Order> getOrdersByUser(User user);
 }
